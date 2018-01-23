4f24275d95fbbe36a0112fa94e4644e4cf016e8a
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2018-01-23 19:01:11 +0300

    Add all files
    - Add code that manages favorite movies without persisting data

--- favorite-movie-app/Movie.swift
+++ favorite-movie-app/Movie.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+class Movie {
+    var id: String = ""
+    var title: String = ""
+    var year: String = ""
+    var imageUrl: String = ""
+    var plot: String = ""
+    
+    init(id: String, title: String, year: String, imageUrl: String, plot: String = "")
+    {
+        self.id = id
+        self.title = title
+        self.year = year
+        self.imageUrl = imageUrl
+        self.plot = plot
+    }
+}
--- favorite-movie-app/ViewController.swift
+++ favorite-movie-app/ViewController.swift
@@ -8,7 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var favoriteMovies: [Movie] = []
+    
+    @IBOutlet var mainTableView: UITableView!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "searchMoviesSegue" {
+            let controller = segue.destination as! SearchViewController
+            controller.delegate = self
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return favoriteMovies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let moviecell = tableView.dequeueReusableCell(withIdentifier: "customcell",
+                                                      for: indexPath) as! CustomTableViewCell
+        let idx: Int = indexPath.row
+        
+        moviecell.movieTitle?.text = favoriteMovies[idx].title
+        moviecell.movieYear?.text = favoriteMovies[idx].year
+        
+        displayMovieImage(idx, moviecell: moviecell)
+        return moviecell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 105 
+    }
+    
+    func displayMovieImage(_ row: Int, moviecell: CustomTableViewCell) {
+        let url: String = (URL(string: favoriteMovies [row].imageUrl)?.absoluteString)!
+        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { (data,
+            response, error) -> Void in
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            DispatchQueue.main.async(execute: {
+                let image = UIImage(data: data!)
+                moviecell.movieImageView?.image = image
+            })
+        }).resume()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        mainTableView.reloadData()
+        if favoriteMovies.count == 0 {
+            favoriteMovies.append(Movie(id: "m000001", title: "Coco", year: "2017", imageUrl: "https://i.pinimg.com/originals/48/6d/84/486d84da85de346d0a007af688f4ed31.jpg"))
+        }
+        super.viewWillAppear(animated)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
