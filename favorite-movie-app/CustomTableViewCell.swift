24686c049743a0c85d003b6434ffc00f084a1ed1
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2018-01-23 18:58:57 +0300

    Initial Commit
    - Add source code for the favorite movie application working  with non persistent data

---
+++ favorite-movie-app/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  favorite-movie-app
+//
+//  Created by Simon Peter Ssekamatte on 1/17/18.
+//  Copyright © 2018 sp_ssekamatte. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ favorite-movie-app/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  favorite-movie-app
+//
+//  Created by Simon Peter Ssekamatte on 1/18/18.
+//  Copyright © 2018 sp_ssekamatte. All rights reserved.
+//
+
+import Foundation
